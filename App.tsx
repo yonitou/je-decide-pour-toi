3b8e4f21b9895964b2d5bb30bf7b6736117962f1
Yoni Touboul <yonitouboul@hotmail.com>
2022-10-09 23:18:07 +0200

    Fixed expo publish

--- App.tsx
+++ App.tsx
@@ -1,6 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
 
 export default (): JSX.Element => {
+	const checkUpdate = async (): Promise<void> => {
+		try {
+			const { isAvailable } = await Updates.checkForUpdateAsync();
+
+			if (isAvailable) {
+				await Updates.fetchUpdateAsync();
+				await Updates.reloadAsync();
+			}
+		} catch (error) {
+			//
+		}
+	};
+
+	useEffect(() => {
+		checkUpdate();
+	}, []);
+
 	return (
 		<View style={styles.container}>
 			<Text>Coucou ghita</Text>
